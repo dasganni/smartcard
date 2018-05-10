c1ddb1a745ee613f065c7a4259a89952d41b39a5
dasganni <20495026+dasganni@users.noreply.github.com>
2018-05-10 15:15:32 +0200

    ups, develop, was solls... further gui development with placeholders

--- app/src/main/java/info/smartcard_haw/smartcard/lobbyActivity.kt
+++ app/src/main/java/info/smartcard_haw/smartcard/lobbyActivity.kt
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_lobby.*
 
@@ -16,17 +17,19 @@ class lobbyActivity : AppCompatActivity() {
 
         //declare views
 
-        var scanQRCodeBtn = findViewById<Button>(R.id.scanQRCodeBtn)
+        var scanQRCodeBtn = findViewById<Button>(R.id.scanViewBtn)
         var waitScanBtn = findViewById<Button>(R.id.waitScanBtn)
-        var codeScannedBtn = findViewById<Button>(R.id.codeScannedBtn)
-        var ergebnisBtn = findViewById<Button>(R.id.ergebnisBtn)
+        var codeScannedBtn = findViewById<Button>(R.id.enterViewBtn)
+        var ergebnisBtn = findViewById<Button>(R.id.showViewBtn)
         var internalInfoTextView = findViewById<TextView>(R.id.internalInfoTextView)
         var placeholderScannedBtn = findViewById<Button>(R.id.placeholderScannedBtn)
         var toEnterTextView = findViewById<TextView>(R.id.toEnterTextView)
         var scanTextView = findViewById<TextView>(R.id.scanTextView)
-
-
-
+        var answerTextBox = findViewById<EditText>(R.id.answerTextBox)
+        var answerHereTextView = findViewById<TextView>(R.id.answerHereTextView)
+        var placeholderAllAnswerSendBtn = findViewById<Button>(R.id.sendAnswerBtn)
+        var waitInfoTextView = findViewById<TextView>(R.id.waitInfoTextView)
+        var placeholderWaitingFinishedBtn = findViewById<Button>(R.id.placeholderWaitingFinishedBtn)
 
 
         //only show the start of the lobby
@@ -40,29 +43,41 @@ class lobbyActivity : AppCompatActivity() {
     fun hideAll(){
 
         //startview
-        scanQRCodeBtn.visibility = View.GONE
+        scanViewBtn.visibility = View.GONE
         waitScanBtn.visibility = View.GONE
-        codeScannedBtn.visibility = View.GONE
-        ergebnisBtn.visibility = View.GONE
+        enterViewBtn.visibility = View.GONE
+        showViewBtn.visibility = View.GONE
         internalInfoTextView.visibility = View.GONE
 
-        //scanview
+        //scanview (einlesen)
         placeholderScannedBtn.visibility = View.GONE
         scanTextView.visibility = View.GONE
 
-        //otherview
+        //waitForScan (warten)
+        waitInfoTextView.visibility = View.GONE
+        placeholderWaitingFinishedBtn.visibility = View.GONE
+
+        //enterView (eingeben)
         toEnterTextView.visibility = View.GONE
+        answerTextBox.visibility = View.GONE
+        answerHereTextView.visibility = View.GONE
+        sendAnswerBtn.visibility = View.GONE
+
+
+
+
+
+        //showView(Einfüllen/Tabelle)
 
 
     }
 
     fun showOnlyLobbyStart(){
         hideAll()
-
-        scanQRCodeBtn.visibility = View.VISIBLE
+        scanViewBtn.visibility = View.VISIBLE
         waitScanBtn.visibility = View.VISIBLE
-        codeScannedBtn.visibility = View.VISIBLE
-        ergebnisBtn.visibility = View.VISIBLE
+        enterViewBtn.visibility = View.VISIBLE
+        showViewBtn.visibility = View.VISIBLE
         internalInfoTextView.visibility = View.VISIBLE
 
 
@@ -70,11 +85,32 @@ class lobbyActivity : AppCompatActivity() {
 
     fun showScanCard(view: View){
         hideAll()
-
         placeholderScannedBtn.visibility = View.VISIBLE
         scanTextView.visibility = View.VISIBLE
 
     }
+
+    fun showWaitView(view: View){
+        hideAll()
+        waitInfoTextView.visibility = View.VISIBLE
+        placeholderWaitingFinishedBtn.visibility = View.VISIBLE
+
+    }
+
+
+
+    fun showEnterView(view: View){
+        hideAll()
+        toEnterTextView.visibility = View.VISIBLE
+        answerTextBox.visibility = View.VISIBLE
+        answerHereTextView.visibility = View.VISIBLE
+        sendAnswerBtn.visibility = View.VISIBLE
+
+    }
+
+
+
+
 
 
 }
