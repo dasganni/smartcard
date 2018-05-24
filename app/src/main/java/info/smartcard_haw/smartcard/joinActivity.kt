013d6c7d2cac7e8378b90543942f4bbcb107a1eb
dasganni <20495026+dasganni@users.noreply.github.com>
2018-05-24 20:21:43 +0200

    implemented QR-Scanner into the App, to pass lobbycode to Lobby-Activity; bug in QR-Scanner in Lobby (Can not change View of QR-Scanner because it is used by another thread)

--- app/src/main/java/info/smartcard_haw/smartcard/joinActivity.kt
+++ app/src/main/java/info/smartcard_haw/smartcard/joinActivity.kt
@@ -4,18 +4,170 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
+import android.view.SurfaceView
+
+import android.Manifest
+import android.content.DialogInterface
+import android.content.pm.PackageManager
+import android.os.Build
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.util.SparseArray
+import android.view.SurfaceHolder
+import android.widget.Toast
+
+import com.google.android.gms.vision.CameraSource
+import com.google.android.gms.vision.Detector
+import com.google.android.gms.vision.barcode.Barcode
+import com.google.android.gms.vision.barcode.BarcodeDetector
+
+import android.Manifest.permission.CAMERA
+import android.os.Looper
+import android.util.Log
+import android.util.LogPrinter
+
+import java.io.IOException
+import info.smartcard_haw.smartcard.R.id.textView
+
+
+
 
 class joinActivity : AppCompatActivity() {
+
+    private val REQUEST_CAMERA = 1
+    var barCodeString : String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_join)
+        val surfaceView : SurfaceView = findViewById(R.id.cameraPreview)
 
-        setTitle("Beitreten")
+        val barcodeDetector = BarcodeDetector.Builder(this)
+                .setBarcodeFormats(Barcode.QR_CODE).build()
+
+        val cameraSource = CameraSource.Builder(this, barcodeDetector)
+                .setRequestedPreviewSize(640, 480)//.build();
+                .setAutoFocusEnabled(true).build()
+
+
+        surfaceView.getHolder().addCallback(object : SurfaceHolder.Callback {
+            override fun surfaceCreated(holder: SurfaceHolder) {
+
+                if (ActivityCompat.checkSelfPermission(applicationContext, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+
+                    return
+
+                }
+                try {
+
+                    cameraSource.start(holder)
+
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+
+            }
+
+
+            override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {
+
+            }
+
+            override fun surfaceDestroyed(holder: SurfaceHolder) {
+
+                cameraSource.stop()
+
+            }
+        })
+
+        barcodeDetector.setProcessor(object : Detector.Processor<Barcode> {
+            override fun release() {
+
+            }
+
+            override fun receiveDetections(detections: Detector.Detections<Barcode>) {
+
+                val qrCodes = detections.detectedItems
+
+                if (qrCodes.size() != 0) {
+
+                    barCodeString = qrCodes.valueAt(0).displayValue
+
+                    //Vibrator vibrator = (Vibrator) getApplicationContext().getSystemService(Context.VIBRATOR_SERVICE);
+                    //vibrator.vibrate(1);
+
+                    scannedCode()
+
+                }
+
+            }
+        })
+
+        val currentApiVersion = Build.VERSION.SDK_INT
+
+        if (currentApiVersion >= Build.VERSION_CODES.M) {
+            if (checkPermission()) {
+                Toast.makeText(applicationContext, "Permission to Camera already granted!", Toast.LENGTH_LONG).show()
+            } else {
+                requestPermission()
+            }
+        }
     }
 
-    fun scannedCode(view: View){
+    private fun checkPermission(): Boolean {
+        return ContextCompat.checkSelfPermission(applicationContext, CAMERA) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(this, arrayOf(CAMERA), REQUEST_CAMERA)
+    }
+
+
+    //Permission für die Benutzung der Kamera vom User abfragen
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            REQUEST_CAMERA -> if (grantResults.size > 0) {
+
+                val cameraAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED
+                if (cameraAccepted) {
+                    Toast.makeText(applicationContext, "Permission Granted", Toast.LENGTH_LONG).show()
+                } else {
+                    Toast.makeText(applicationContext, "Permission Denied", Toast.LENGTH_LONG).show()
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        if (shouldShowRequestPermissionRationale(CAMERA)) {
+                            showMessageOKCancel("You need to allow access to both the permissions",
+                                    DialogInterface.OnClickListener { dialog, which ->
+                                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                                            requestPermissions(arrayOf(CAMERA),
+                                                    REQUEST_CAMERA)
+                                        }
+                                    })
+                            return
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private fun showMessageOKCancel(message: String, okListener: DialogInterface.OnClickListener) {
+        android.support.v7.app.AlertDialog.Builder(this)
+                .setMessage(message)
+                .setPositiveButton("OK", okListener)
+                .setNegativeButton("Cancel", null)
+                .create()
+                .show()
+    }
+
+
+
+
+    fun scannedCode(){
+
         val scannedIntent = Intent(this, lobbyActivity::class.java)
+            scannedIntent.putExtra("barCodeStringExtra", barCodeString)
         startActivity(scannedIntent)
     }
 }
--- app/src/main/java/info/smartcard_haw/smartcard/lobbyActivity.kt
+++ app/src/main/java/info/smartcard_haw/smartcard/lobbyActivity.kt
@@ -1,23 +1,135 @@
 package info.smartcard_haw.smartcard
 
+import android.Manifest
+import android.content.DialogInterface
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.constraint.ConstraintLayout
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.view.SurfaceHolder
+import android.view.SurfaceView
 import android.view.View
 import android.widget.*
 import android.widget.TableRow.LayoutParams
 import kotlinx.android.synthetic.main.activity_lobby.*
 import android.widget.TableLayout
+import com.google.android.gms.vision.CameraSource
+import com.google.android.gms.vision.Detector
+import com.google.android.gms.vision.barcode.Barcode
+import com.google.android.gms.vision.barcode.BarcodeDetector
+import java.io.IOException
+import info.smartcard_haw.smartcard.R.id.textView
+
 
 
 
 class lobbyActivity : AppCompatActivity() {
 
+    private val REQUEST_CAMERA = 1
+    var barCodeString : String? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lobby)
+        var lobbyCodeScan = intent.getStringExtra("barCodeStringExtra")
 
-        setTitle("Lobby")
+        setTitle("Lobby-ID: $lobbyCodeScan")
+
+        //declare views
+
+        var scanViewBtn = findViewById<Button>(R.id.scanViewBtn)
+        var waitScanBtn = findViewById<Button>(R.id.waitScanBtn)
+        var codeScannedBtn = findViewById<Button>(R.id.enterViewBtn)
+        var showViewBtn = findViewById<Button>(R.id.showViewBtn)
+        var internalInfoTextView = findViewById<TextView>(R.id.internalInfoTextView)
+        var surfaceView = findViewById<SurfaceView>(R.id.surfaceView)
+        var toEnterTextView = findViewById<TextView>(R.id.toEnterTextView)
+        var scanTextView = findViewById<TextView>(R.id.scanTextView)
+        var answerTextBox = findViewById<EditText>(R.id.answerTextBox)
+        var answerHereTextView = findViewById<TextView>(R.id.answerHereTextView)
+        var placeholderAllAnswerSendBtn = findViewById<Button>(R.id.sendAnswerBtn)
+        var waitInfoTextView = findViewById<TextView>(R.id.waitInfoTextView)
+        var placeholderWaitingFinishedBtn = findViewById<Button>(R.id.placeholderWaitingFinishedBtn)
+        var tableTL = findViewById<TableLayout>(R.id.tableTL)
+        var answersTextView = findViewById<TextView>(R.id.answersTextView)
+
+        //Barcode-Scanner
+
+        val barcodeDetector = BarcodeDetector.Builder(this)
+                .setBarcodeFormats(Barcode.QR_CODE).build()
+
+        val cameraSource = CameraSource.Builder(this, barcodeDetector)
+                .setRequestedPreviewSize(640, 480)//.build();
+                .setAutoFocusEnabled(true).build()
+
+
+        surfaceView.getHolder().addCallback(object : SurfaceHolder.Callback {
+            override fun surfaceCreated(holder: SurfaceHolder) {
+
+                if (ActivityCompat.checkSelfPermission(applicationContext, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+
+                    return
+
+                }
+                try {
+
+                    cameraSource.start(holder)
+
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                }
+
+            }
+
+
+            override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {
+
+            }
+
+            override fun surfaceDestroyed(holder: SurfaceHolder) {
+
+                cameraSource.stop()
+
+            }
+        })
+
+        barcodeDetector.setProcessor(object : Detector.Processor<Barcode> {
+            override fun release() {
+
+            }
+
+            override fun receiveDetections(detections: Detector.Detections<Barcode>) {
+
+                val qrCodes = detections.detectedItems
+
+                if (qrCodes.size() != 0) {
+
+                    barCodeString = qrCodes.valueAt(0).displayValue
+                    cameraSource.stop()
+                    barcodeDetector.release()
+                    scannedCode()
+
+                }
+
+            }
+        })
+
+
+        val currentApiVersion = Build.VERSION.SDK_INT
+
+        if (currentApiVersion >= Build.VERSION_CODES.M) {
+            if (checkPermission()) {
+                Toast.makeText(applicationContext, "Permission to Camera already granted!", Toast.LENGTH_LONG).show()
+            } else {
+                requestPermission()
+            }
+        }
+
 
 
         //Platzhalter-Parameter bis Lobby fertig ist
@@ -44,31 +156,6 @@ class lobbyActivity : AppCompatActivity() {
         players[2].speed=2.5
         players[3].speed=3.0
         players[4].speed=20.1
-
-
-
-
-
-
-
-        //declare views
-
-        var scanViewBtn = findViewById<Button>(R.id.scanViewBtn)
-        var waitScanBtn = findViewById<Button>(R.id.waitScanBtn)
-        var codeScannedBtn = findViewById<Button>(R.id.enterViewBtn)
-        var showViewBtn = findViewById<Button>(R.id.showViewBtn)
-        var internalInfoTextView = findViewById<TextView>(R.id.internalInfoTextView)
-        var placeholderScannedBtn = findViewById<Button>(R.id.placeholderScannedBtn)
-        var toEnterTextView = findViewById<TextView>(R.id.toEnterTextView)
-        var scanTextView = findViewById<TextView>(R.id.scanTextView)
-        var answerTextBox = findViewById<EditText>(R.id.answerTextBox)
-        var answerHereTextView = findViewById<TextView>(R.id.answerHereTextView)
-        var placeholderAllAnswerSendBtn = findViewById<Button>(R.id.sendAnswerBtn)
-        var waitInfoTextView = findViewById<TextView>(R.id.waitInfoTextView)
-        var placeholderWaitingFinishedBtn = findViewById<Button>(R.id.placeholderWaitingFinishedBtn)
-        var tableTL = findViewById<TableLayout>(R.id.tableTL)
-        var answersTextView = findViewById<TextView>(R.id.answersTextView)
-
 
 
         //generate table for count of players
@@ -113,9 +200,70 @@ class lobbyActivity : AppCompatActivity() {
             tableTextViewsOuter.add(tableTextViewsInner)
         }
 
+        //Nach öffnen der Activity nur improvisierten Start zeigen
+
         showOnlyLobbyStart()
 
     }
+
+    //Prüfe Kamera-Rechte
+    private fun checkPermission(): Boolean {
+        return ContextCompat.checkSelfPermission(applicationContext, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED
+    }
+
+    //Frage nach Rechte für Kamera
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), REQUEST_CAMERA)
+    }
+
+
+    //Permission für die Benutzung der Kamera vom User abfragen
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            REQUEST_CAMERA -> if (grantResults.size > 0) {
+
+                val cameraAccepted = grantResults[0] == PackageManager.PERMISSION_GRANTED
+                if (cameraAccepted) {
+                    Toast.makeText(applicationContext, "Permission Granted", Toast.LENGTH_LONG).show()
+                } else {
+                    Toast.makeText(applicationContext, "Permission Denied", Toast.LENGTH_LONG).show()
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                        if (shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)) {
+                            showMessageOKCancel("You need to allow access to both the permissions",
+                                    DialogInterface.OnClickListener { dialog, which ->
+                                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                                            requestPermissions(arrayOf(Manifest.permission.CAMERA),
+                                                    REQUEST_CAMERA)
+                                        }
+                                    })
+                            return
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    //Fenster für Rechte-Abfrage
+    private fun showMessageOKCancel(message: String, okListener: DialogInterface.OnClickListener) {
+        android.support.v7.app.AlertDialog.Builder(this)
+                .setMessage(message)
+                .setPositiveButton("OK", okListener)
+                .setNegativeButton("Cancel", null)
+                .create()
+                .show()
+    }
+
+
+
+    //QR-Code wurde gescannt
+    fun scannedCode(){
+
+        showEnterView()
+    }
+
+
 
     //function to hide all views and then show specific views with another function
 
@@ -129,7 +277,7 @@ class lobbyActivity : AppCompatActivity() {
         internalInfoTextView.visibility = View.GONE
 
         //scanview (einlesen)
-        placeholderScannedBtn.visibility = View.GONE
+        surfaceView.visibility = View.GONE
         scanTextView.visibility = View.GONE
 
         //waitForScan (warten)
@@ -147,13 +295,6 @@ class lobbyActivity : AppCompatActivity() {
         answersTextView.visibility = View.GONE
 
 
-
-
-
-
-        //showView(Einfüllen/Tabelle)
-
-
     }
 
     fun showOnlyLobbyStart(){
@@ -169,7 +310,7 @@ class lobbyActivity : AppCompatActivity() {
 
     fun showScanCard(view: View){
         hideAll()
-        placeholderScannedBtn.visibility = View.VISIBLE
+        surfaceView.visibility = View.VISIBLE
         scanTextView.visibility = View.VISIBLE
 
     }
@@ -185,6 +326,20 @@ class lobbyActivity : AppCompatActivity() {
 
     fun showEnterView(view: View){
         hideAll()
+        toEnterTextView.visibility = View.VISIBLE
+        answerTextBox.visibility = View.VISIBLE
+        answerHereTextView.visibility = View.VISIBLE
+        sendAnswerBtn.visibility = View.VISIBLE
+
+    }
+
+    fun showEnterView(){
+        hideAll()
+        toEnterTextView.post(Runnable {
+            //Vibrator vibrator = (Vibrator) getApplicationContext().getSystemService(Context.VIBRATOR_SERVICE);
+            //vibrator.vibrate(1);
+            toEnterTextView.text = "Yeah, du hast eine Karte gescannt, das steht drauf: $barCodeString"
+        })
         toEnterTextView.visibility = View.VISIBLE
         answerTextBox.visibility = View.VISIBLE
         answerHereTextView.visibility = View.VISIBLE
